7df9664990756b035f140281539ce3acff20c2ff
busramerve37 <145451633+busramerve37@users.noreply.github.com>
2023-10-16 14:31:52 +0300

    Add files via upload

---
+++ button.ornek.28/button.ornek.28/button.ornek.28/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace button.ornek._28
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int gunNo = comboBox1.SelectedIndex;
+            gunNo++;
+            if (gunNo==1 || gunNo==2 || gunNo==3 || gunNo==4 || gunNo == 5)
+            {
+                label2.Text = "Çalışma Saati 09:00-17:00 arasıdır";
+            }
+            else if (gunNo==6)
+                {
+                label2.Text = "Çalışma Saati 09:00-13:00 arasıdır";
+            }
+            
+
+                
+            
+            else
+            {
+                label2.Text = "Tatil";
+            }
+        }
+    }
+}
